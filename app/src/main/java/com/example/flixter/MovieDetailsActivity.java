f2f3ecf9c23a8adbc05c7d2bae71f56fd07afe60
vdabro2 <73124810+vdabro2@users.noreply.github.com>
2022-06-03 11:02:38 -0700

    added intent

--- app/src/main/java/com/example/flixter/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixter/MovieDetailsActivity.java
@@ -3,12 +3,19 @@ package com.example.flixter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.util.Log;
+
+import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
-
+    Movie movie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
     }
 }
