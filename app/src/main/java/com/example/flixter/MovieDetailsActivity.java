94a17503476407782d0a53f921bd7f2cfcedfbbe
vdabro2 <73124810+vdabro2@users.noreply.github.com>
2022-06-03 15:33:13 -0700

    Attempted to add YT videos

--- app/src/main/java/com/example/flixter/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixter/MovieDetailsActivity.java
@@ -8,9 +8,14 @@ import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 import org.parceler.Parcels;
 
-public class MovieDetailsActivity extends AppCompatActivity {
+public class MovieDetailsActivity extends YouTubeBaseActivity {
     Movie movie;
 
     TextView tvTitle2;
@@ -18,6 +23,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     RatingBar rbVoteAverage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //https://api.themoviedb.org/3/movie/338953/videos?api_key=9cf5af5129f4f863d5cf57a867a4084c&language=en-US
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
@@ -35,5 +41,24 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage / 2.0f);
+
+        YouTubePlayerView youTubePlayerView =
+                (YouTubePlayerView) findViewById(R.id.player);
+
+        youTubePlayerView.initialize("YOUR API KEY",
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+
+                        // do any work here to cue video, play video, etc.
+                        youTubePlayer.cueVideo("5xVh-7ywKpE");
+                    }
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+
+                    }
+                });
     }
 }
